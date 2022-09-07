375dfbc67cd6a02435089276060ce6c42a25ace4
Jonas Augusto <jonasaugusto99@gmail.com>
2022-09-07 15:40:43 -0300

    Add routes

---
+++ src/routes/post.ts
@@ -0,0 +1,18 @@
+import {Router} from "express"
+
+const router = Router()
+
+// Get ALl
+router.get("/", )
+
+// Get by ID
+router.get("/:id", )
+
+router.post("/")
+
+router.put("/:id")
+
+router.delete('/:id')
+
+export default router
+
